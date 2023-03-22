9592f26d3ed884ffda5b99cc59a1f416aac117d2
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-03-22 15:27:45 -0600

    uses static middleware instead of three endpoints

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html')
+// })
 
-app.get('/css', (req,res)=>{
-    res.sendFile(__dirname + '/public/styles.css')
-})
+// app.get('/css', (req,res)=>{
+//     res.sendFile(__dirname + '/public/styles.css')
+// })
 
-app.get('/js', (req,res)=>{
-    res.sendFile(__dirname + '/public/main.js')
-})
+// app.get('/js', (req,res)=>{
+//     res.sendFile(__dirname + '/public/main.js')
+// })
 
+//this is the same as the three app.gets above
+app.use(express.static(`${__dirname}/public`))
 
 
 app.listen(4000, () => {
